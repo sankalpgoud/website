198606f954844a4f3bb8ca6146c481bc54c4960e
sankalpgoud <140591520+sankalpgoud@users.noreply.github.com>
2023-12-05 11:33:40 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask,render_template
 
-app=Flask(__name__)
-
+app = Flask(__name__)
 @app.route("/")
 def helloworld():
-  return"hello bot "
+  return render_template('p.html')
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
