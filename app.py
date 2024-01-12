1ab170100c31de474741309d4cb7303758c7f5a3
sankalpgoud <140591520+sankalpgoud@users.noreply.github.com>
2024-01-12 11:08:19 +0000

    added everything

--- app.py
+++ app.py
@@ -1,9 +1,41 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
+hashira=[
+  {
+  'id': 1,
+  'name': 'Rengoku',
+  'Breathing style': 'flame',
+  'age': 20
+  },
+  
+  {
+    'id': 2,
+    'name': 'Shinobu Kochu',
+    'Breathing style': 'Insect',
+    'age': 18
+    },
+  {
+    'id': 3,
+    'name': 'Tokito',
+    'Breathing style': 'Mist',
+    'age': 14
+    },
+  {
+    'id': 4,
+    'name': 'Tomioka',
+    'Breathing style': 'Water',
+    'age': 21
+    }
+]
+
 @app.route("/")
 def helloworld():
-  return render_template('p.html')
+  return render_template('p.html',hash=hashira,
+                         name='Mitsuri')
+@app.route("/api/hashira")
+def _list():
+  return jsonify(hashira)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
